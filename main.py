abbafba6889e645af4533b34d9fa88d44178f81c
AZAZ3LL0 <101965763+AZAZ3LL0@users.noreply.github.com>
2022-11-20 12:05:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
 
 
-def f(x):
+def y(x):
     f = math.exp(-2*x) + math.sqrt(3*x) + math.cos(-9*x) - 5*x**1.5
     return f
 
@@ -9,11 +9,11 @@ def f(x):
 down_limit = int(input("Введите нижнюю границу интеграла:"))
 up_limit = int(input("Введите верхнюю границу интеграла:"))
 n = int(input("Введите количество интервалов:"))
-dx = ((up_limit - down_limit) / n)
+dx: float = ((up_limit - down_limit) / n)
 integral = 0
 
 for i in range(1, n):
-    integral = integral + dx * f(i * dx)
+    integral = integral + dx * y(i * dx)
 
 print("Значение определённого интеграла = ", integral)
 print("Точность " + str((integral/n) /integral) + '%')
